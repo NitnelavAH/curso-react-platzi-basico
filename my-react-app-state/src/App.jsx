9d36fa47845e6c0c9a12dfa73825ee28d3561be0
Jose Valentin Abundo Hernandez <valentin.abundo@bambu-mobile.com>
2025-06-02 22:10:33 -0600

    feat(states): add all components to the view

--- my-react-app-state/src/App.jsx
+++ my-react-app-state/src/App.jsx
@@ -1,10 +1,23 @@
-import CounterWithEffect from "./components/CounterWithEffect";
+import Counter from './components/Counter';
+import ToggleButton from './components/ToggleButton';
+import NameForm from './components/NameForm';
+import CounterWithEffect from './components/CounterWithEffect';
 
 import './App.css'
 
 function App() {
   return (
     <>
+      <h2>Counter</h2>
+      <Counter />
+      
+      <h2>Toggle</h2>
+      <ToggleButton />
+      
+      <h2>Form</h2>
+      <NameForm />
+
+      <h2>Conter With Effect</h2>
       <CounterWithEffect />
     </>
   )
